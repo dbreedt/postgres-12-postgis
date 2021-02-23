48d2e99db527707c69b3e06f9f6b235aa678b932
Derik Breedt <dbreedt@retrorabbit.co.za>
2021-02-23 13:55:44 +0200

    Changed comment, plus params from pgtune. Added worker lines

--- setup_pg.sh
+++ setup_pg.sh
@@ -8,24 +8,26 @@ set -e
   create extension IF NOT EXISTS postgis_topology;
 EOSQL
 
-# values from pgtune 2GB RAM + 1CPU + SDD + WebApplication
+# values from pgtune 2GB RAM + 2CPU + 20connections + SDD + Oltp
 
 echo fsync=off >> /var/lib/postgresql/data/postgresql.conf
 echo synchronous_commit=off >> /var/lib/postgresql/data/postgresql.conf
 echo full_page_writes=off >> /var/lib/postgresql/data/postgresql.conf
-echo work_mem=20MB >> /var/lib/postgresql/data/postgresql.conf
-echo max_connections=100 >> /var/lib/postgresql/data/postgresql.conf
+echo work_mem=30MB >> /var/lib/postgresql/data/postgresql.conf
+echo max_connections=20 >> /var/lib/postgresql/data/postgresql.conf
 echo shared_buffers=512MB >> /var/lib/postgresql/data/postgresql.conf
-echo effective_cache_size=1500MB >> /var/lib/postgresql/data/postgresql.conf
+echo effective_cache_size=1536MB >> /var/lib/postgresql/data/postgresql.conf
 echo archive_mode=off >> /var/lib/postgresql/data/postgresql.conf
 echo maintenance_work_mem=128MB >> /var/lib/postgresql/data/postgresql.conf
-echo checkpoint_completion_target=0.7 >> /var/lib/postgresql/data/postgresql.conf
+echo checkpoint_completion_target=0.9 >> /var/lib/postgresql/data/postgresql.conf
 echo random_page_cost=1.1 >> /var/lib/postgresql/data/postgresql.conf
-echo effective_io_concurrency=300 >> /var/lib/postgresql/data/postgresql.conf
+echo effective_io_concurrency=200 >> /var/lib/postgresql/data/postgresql.conf
 echo max_wal_senders=0 >> /var/lib/postgresql/data/postgresql.conf
 echo wal_level=minimal >> /var/lib/postgresql/data/postgresql.conf
 echo wal_buffers=16MB >> /var/lib/postgresql/data/postgresql.conf
 echo min_wal_size=32MB >> /var/lib/postgresql/data/postgresql.conf
-echo max_wal_size=200MB >> /var/lib/postgresql/data/postgresql.conf
-echo timezone=UTC >> /var/lib/postgresql/data/postgresql.conf
-
+echo max_wal_size=256MB >> /var/lib/postgresql/data/postgresql.conf
+echo max_worker_processes=2 >> /var/lib/postgresql/data/postgresql.conf
+echo max_parallel_workers_per_gather=1 >> /var/lib/postgresql/data/postgresql.conf
+echo max_parallel_workers=2 >> /var/lib/postgresql/data/postgresql.conf
+echo max_parallel_maintenance_workers=1 >> /var/lib/postgresql/data/postgresql.conf
